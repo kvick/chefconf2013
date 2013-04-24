7240351db8deac618658cad66b188624e9ec638f
kvick <kvick@netflix.com>
2013-04-24 11:30:02 -0700

    Default recipe creates myface user.

---
+++ files/default/tests/minitest/default_test.rb
@@ -0,0 +1,15 @@
+require File.expand_path('../support/helpers', __FILE__)
+
+describe 'myface::default' do
+
+  include Helpers::Myface
+
+  # Example spec tests can be found at http://git.io/Fahwsw
+  # it 'runs no tests by default' do
+  #end
+
+  # Check if a user has been created
+  it "creates a service account for the myface daemon" do
+    user("myface").must_exist
+  end
+end
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             "myface"
+maintainer       "K. Vick"
+maintainer_email "kvick@netflix.com"
+license          "All rights reserved"
+description      "Installs/Configures myface"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.2.0"
+
